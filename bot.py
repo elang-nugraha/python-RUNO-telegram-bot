ca28b32cad735f8195b1115b1ed0144192dd5ee9
Elang <elangunonugraha11@gmail.com>
2025-08-17 07:34:02 +0700

    set polling interval to 3

--- bot.py
+++ bot.py
@@ -456,4 +456,4 @@ app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 app.add_handler(CommandHandler("getAllData", getAllData))
 
 
-app.run_polling()
+app.run_polling(3)
